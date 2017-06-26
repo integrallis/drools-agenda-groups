64f711cb838c48fbe57f3f4447b004bf2972ec3b
Brian Sam-Bodden <bsbodden@integrallis.com>
2017-06-26 12:41:32 -0700

    Fix Mavenification and Drools 7.0.0.final upgrade

--- src/main/java/org/integrallis/drools/examples/AgendaGroupTest.java
+++ src/main/java/org/integrallis/drools/examples/AgendaGroupTest.java
@@ -1,18 +1,10 @@
 package org.integrallis.drools.examples;
 
-import org.drools.KnowledgeBase;
-import org.drools.KnowledgeBaseFactory;
-import org.drools.builder.KnowledgeBuilder;
-import org.drools.builder.KnowledgeBuilderError;
-import org.drools.builder.KnowledgeBuilderErrors;
-import org.drools.builder.KnowledgeBuilderFactory;
-import org.drools.builder.ResourceType;
-import org.drools.io.ResourceFactory;
-import org.drools.logger.KnowledgeRuntimeLogger;
-import org.drools.logger.KnowledgeRuntimeLoggerFactory;
-import org.drools.runtime.StatefulKnowledgeSession;
-import org.drools.runtime.rule.QueryResults;
-import org.drools.runtime.rule.QueryResultsRow;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.QueryResults;
+import org.kie.api.runtime.rule.QueryResultsRow;
 
 /**
  * This is a sample class to launch a rule.
@@ -20,49 +12,35 @@ import org.drools.runtime.rule.QueryResultsRow;
 public class AgendaGroupTest {
 
 	public static final void main(String[] args) {
+		KieSession knowledgeSession = null;
 		try {
 			// load up the knowledge base
-			KnowledgeBase kbase = readKnowledgeBase();
-			StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
-			KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "test");
+	        // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+		    KieContainer kContainer = ks.getKieClasspathContainer();
+		    knowledgeSession = kContainer.newKieSession("ksession-rules");
+		    
 			// go !
 			String[] agendaGroups = {"Group One", "Group Two" };
 			TestObject testObjectA = new TestObject("A");
 			TestObject testObjectB = new TestObject("B");
 			for (String group : agendaGroups) {
 				System.out.println("Running with " + group + " group in focus");
-				ksession.insert(testObjectA);
-				ksession.insert(testObjectB);
-				ksession.getAgenda().getAgendaGroup(group).setFocus();
-				ksession.fireAllRules();
+				knowledgeSession.insert(testObjectA);
+				knowledgeSession.insert(testObjectB);
+				knowledgeSession.getAgenda().getAgendaGroup(group).setFocus();
+				knowledgeSession.fireAllRules();
 			}
 			
-			QueryResults results = ksession.getQueryResults( "FindClassNameStartingWith", new Object[] { TestObject.class } );
+			QueryResults results = knowledgeSession.getQueryResults( "FindClassNameStartingWith", new Object[] { TestObject.class } );
 			
 			for ( QueryResultsRow row : results ) {
 				TestObject testObject = (TestObject) row.get( "object" );
 				System.out.println("Found " + testObject);
 			}
-			
-			logger.close();
 		} catch (Throwable t) {
 			t.printStackTrace();
 		}
-	}
-
-	private static KnowledgeBase readKnowledgeBase() throws Exception {
-		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-		kbuilder.add(ResourceFactory.newClassPathResource("sample_rules.drl"), ResourceType.DRL);
-		KnowledgeBuilderErrors errors = kbuilder.getErrors();
-		if (errors.size() > 0) {
-			for (KnowledgeBuilderError error: errors) {
-				System.err.println(error);
-			}
-			throw new IllegalArgumentException("Could not parse knowledge.");
-		}
-		KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
-		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
-		return kbase;
 	}
 
 }
